e8218ac85dddc1cfa29c780b49686a4e0cf9678f
kaivan shah <kaivanshah1663@gmail.com>
2018-09-16 01:53:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var socket = require('socket.io')
+var app = express();
+app.set('view engine','ejs')
+
+
+app.use(express.static('public'));
+
+
+var server = app.listen(3000,function(req,res)
+{
+    console.log("Server is responding..");
+});
+
+var io = socket(server);
+io.on('connection',function(socket){
+    console.log('Made socket connection')
+
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data);
+    });
+    socket.on('typing',function(name){
+        socket.broadcast.emit("typing",name);
+    });
+});
