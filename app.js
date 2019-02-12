8bbebe3dcb46e37236bfeb571b51b9ab8b173c46
Kaivan Shah <32458209+kaivan-s@users.noreply.github.com>
2019-02-13 00:55:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ var express = require('express')
 var socket = require('socket.io')
 var app = express();
 app.set('view engine','ejs')
-
-
 app.use(express.static('public'));
-
-
 var server = app.listen(3000,function(req,res)
 {
     console.log("Server is responding..");
 });
 
 var io = socket(server);
-io.on('connection',function(socket){
+io.on('connection',function(socket)
+{
     console.log('Made socket connection')
-
-    socket.on('chat',function(data){
+    socket.on('chat',function(data)
+    {
         io.sockets.emit('chat',data);
     });
-    socket.on('typing',function(name){
+    socket.on('typing',function(name)
+    {
         socket.broadcast.emit("typing",name);
     });
 });
